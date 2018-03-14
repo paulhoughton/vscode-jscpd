609155f17468db106018cf787474738b403ed955
paulhoughton <paulhoughton@users.noreply.github.com>
2018-03-14 07:18:33 +0000

    Update to latest jscpd

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 VSCode extension for the copy/paste detector [jscpd](https://github.com/kucherenko/jscpd)
 
-Detects code duplication in a project. 
+Detects code duplication in a project.
 
 ![Image of jscpd](https://github.com/paulhoughton/vscode-jscpd/raw/master/images/demo.gif)
 
@@ -11,5 +11,8 @@ Detects code duplication in a project.
 This extension contributes the following settings:
 
 * `jscpd.exclude`: Files to ignore
+* `jscpd.files`: glob pattern for find code
+* `jscpd.languagesExts`: list of languages with file extensions
+* `jscpd.skipComments`: skip comments in code
 * `jscpd.minLines`: min size of duplication in code lines
 * `jscpd.minTokens`: min size of duplication in code tokens
--- src/extension.js
+++ src/extension.js
@@ -6,72 +6,118 @@ const showLines = require('./showLines');
 const reportProvider = require('./reportProvider');
 
 function activate(context) {
+  const jscpd_inst = new jscpd();
+  const jscpdSchema = 'jscpd';
 
-    const jscpd_inst = new jscpd();
-    const jscpdSchema = 'jscpd';
+  let provider = new reportProvider();
+  let registration = vscode.workspace.registerTextDocumentContentProvider(
+    jscpdSchema,
+    provider
+  );
 
-    let provider = new reportProvider();
-    let registration = vscode.workspace.registerTextDocumentContentProvider(jscpdSchema, provider);
+  let disposableShowLines = vscode.commands.registerCommand(
+    'jscpd.showLines',
+    showLines
+  );
 
-    let disposableShowLines = vscode.commands.registerCommand('jscpd.showLines', showLines);
+  let disposable = vscode.commands.registerCommand('extension.jscpd', function(
+    explorer
+  ) {
+    const currentPath =
+      (explorer && path.dirname(explorer._fsPath)) || vscode.workspace.rootPath;
 
-    let disposable = vscode.commands.registerCommand('extension.jscpd', function (explorer) {
+    const exclude = vscode.workspace.getConfiguration('jscpd').get('exclude');
+    const files = vscode.workspace.getConfiguration('jscpd').get('files');
+    const minLines = vscode.workspace.getConfiguration('jscpd').get('minLines');
+    const minTokens = vscode.workspace
+      .getConfiguration('jscpd')
+      .get('minTokens');
+    const skipComments = vscode.workspace
+      .getConfiguration('jscpd')
+      .get('skipComments');
+    const languagesExts = vscode.workspace
+      .getConfiguration('jscpd')
+      .get('languagesExts');
+    const languages =
+      vscode.workspace.getConfiguration('jscpd').get('languages') || '';
 
-        const currentPath = (explorer && path.dirname(explorer._fsPath)) || vscode.workspace.rootPath;
+    let config = {
+      reporter: 'json',
+      path: currentPath,
+      exclude,
+      'min-lines': minLines,
+      'min-tokens': minTokens
+    };
 
-        const output = jscpd_inst.run({
-            reporter: "json",
-            path: currentPath,
-            exclude: vscode.workspace.getConfiguration('jscpd').get('exclude'),
-            'min-lines': vscode.workspace.getConfiguration('jscpd').get('minLines'),
-            'min-tokens': vscode.workspace.getConfiguration('jscpd').get('minTokens')
-        })
+    if (files) config['files'] = files;
+    if (skipComments) config['skip-comments'] = skipComments;
+    if (languages) config['languages'] = languages;
+    if (languagesExts) config['languages-ext'] = languagesExts;
 
-        if (output.report.statistics.clones === 0) {
-            vscode.window.showInformationMessage(`0 duplicated lines out of ${output.report.statistics.lines} total lines of code.`);
-            return;
-        }
-        const formatName = format(currentPath);
+    const output = jscpd_inst.run(config);
 
-        let quickPickList = output.map.clones.map(data => ({
-            label: formatName(data.firstFile, data.firstFileStart, data.linesCount) + " <-> " + formatName(data.secondFile, data.secondFileStart, data.linesCount),
-            data
-        }));
+    if (output.report.statistics.clones === 0) {
+      vscode.window.showInformationMessage(
+        `0 duplicated lines out of ${
+          output.report.statistics.lines
+        } total lines of code.`
+      );
+      return;
+    }
+    const formatName = format(currentPath);
 
-        const {percentage, duplications, lines} = output.report.statistics;
+    let quickPickList = output.map.clones.map(data => ({
+      label:
+        formatName(data.firstFile, data.firstFileStart, data.linesCount) +
+        ' <-> ' +
+        formatName(data.secondFile, data.secondFileStart, data.linesCount),
+      data
+    }));
 
-        quickPickList.unshift({
-            label: "",
-            description: `${percentage}% (${duplications} lines) duplicated lines out of ${lines} total lines of code`,
-            data: "REPORT"
-        });
+    const { percentage, duplications, lines } = output.report.statistics;
 
-        vscode.window.showQuickPick(quickPickList).then(({data}) => {
-            if (data === "REPORT") {
-                provider.setPathFormatter(formatName);
-                provider.setData(output.map.clones);
-                var previewUri = vscode.Uri.parse(jscpdSchema + '://authority/jscpd?x=' + new Date().getTime().toString());
-                vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.One, 'JSCPD').then(() => {
-                }, (error) => {
-                    vscode.window.showErrorMessage(error);
-                });
-            }
-            else {
-                vscode.commands.executeCommand("jscpd.showLines", data);
-            }
-        });
+    quickPickList.unshift({
+      label: '',
+      description: `${percentage}% (${duplications} lines) duplicated lines out of ${lines} total lines of code`,
+      data: 'REPORT'
     });
 
-    context.subscriptions.push(disposable, disposableShowLines, registration);
+    vscode.window.showQuickPick(quickPickList).then(({ data }) => {
+      if (data === 'REPORT') {
+        provider.setPathFormatter(formatName);
+        provider.setData(output.map.clones);
+        var previewUri = vscode.Uri.parse(
+          jscpdSchema +
+            '://authority/jscpd?x=' +
+            new Date().getTime().toString()
+        );
+        vscode.commands
+          .executeCommand(
+            'vscode.previewHtml',
+            previewUri,
+            vscode.ViewColumn.One,
+            'JSCPD'
+          )
+          .then(
+            () => {},
+            error => {
+              vscode.window.showErrorMessage(error);
+            }
+          );
+      } else {
+        vscode.commands.executeCommand('jscpd.showLines', data);
+      }
+    });
+  });
+
+  context.subscriptions.push(disposable, disposableShowLines, registration);
 }
 
-const format = (currentPath) => (file, start, count) => {
-    const stripProject = path => path.replace(currentPath + "\\", "")
+const format = currentPath => (file, start, count) => {
+  const stripProject = path => path.replace(currentPath + '\\', '');
 
-    return stripProject(file, currentPath) + ":" + start + "-" + (start + count);
-
-}
-function deactivate() {
-}
+  return stripProject(file, currentPath) + ':' + start + '-' + (start + count);
+};
+function deactivate() {}
 exports.activate = activate;
 exports.deactivate = deactivate;
